dc96777875c0207fe66a66a428fb9e79b8a4dfa5
Fons van der Plas <fonsvdplas@gmail.com>
2021-04-09 00:54:03 +0200

    Support svg show directly, disable html show

--- src/svg.jl
+++ src/svg.jl
@@ -18,13 +18,16 @@ end
 Returns an SVG representation of the given plot as a string.
 """
 function to_svg(plot::PenPlot)
+    repr(MIME"image/svg+xml"(), plot)
+end
+
+Base.show(io::IO, ::MIME"image/svg+xml", plot::PenPlot)
     ext = extent(plot)
     width = ext.lowerright[1] - ext.upperleft[1]
     height = ext.lowerright[2] - ext.upperleft[2]
 
-    builder = IOBuffer()
     print(
-        builder,
+        io,
         """<svg
 xmlns="http://www.w3.org/2000/svg"
 height="400"
@@ -34,7 +37,7 @@ viewBox="$(ext.upperleft[1]) $(ext.upperleft[2]) $(width) $(height)">""",
 
     for layer in plot.layers
         print(
-            builder,
+            io,
             """<g id="$(layer.name)"
 stroke="#$(hex(layer.color))"
 opacity="0.5"
@@ -43,7 +46,7 @@ fill="none">""",
 
         for path in layer.paths
             print(
-                builder,
+                io,
                 """<path
   d="$(to_path_string(path))"
   vector-effect="non-scaling-stroke"
@@ -51,34 +54,18 @@ fill="none">""",
             )
         end
 
-        print(builder, "</g>")
+        print(io, "</g>")
     end
 
-    print(builder, "</svg>")
-
-    String(take!(builder))
+    print(io, "</svg>")
 end
 
-function Base.show(io::IO, mime::MIME"text/html", path::Path)
+function Base.show(io::IO, mime::MIME"image/svg+xml", path::Path)
     plot = PenPlot([path])
     Base.show(io, mime, plot)
 end
 
-function Base.show(io::IO, mime::MIME"text/html", paths::MultiPath)
+function Base.show(io::IO, mime::MIME"image/svg+xml", paths::MultiPath)
     plot = PenPlot(paths)
     Base.show(io, mime, plot)
-end
-
-function Base.show(io::IO, mime::MIME"text/html", plot::PenPlot)
-    svgdata = to_svg(plot)
-
-    # Write SVG element.
-    write(io, svgdata)
-
-    # Write download button.
-    data_encoded = base64encode(svgdata)
-    url = "data:image/svg+xml;base64,$(data_encoded)"
-    write(io, "<div>")
-    write(io, """<a href="$(url)" download="plot.svg"><button>Download</button></a>""")
-    write(io, "</div>")
 end
